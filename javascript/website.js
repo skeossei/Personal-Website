ebe1e28c4298d3d60cd6768e803673bbe3591b02
Sevag Keosseian <skeossei@ucsd.edu>
2019-09-08 17:04:45 -0700

    change init page name

--- javascript/website.js
+++ javascript/website.js
@@ -6,7 +6,7 @@ const hostname = '127.0.0.1';
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  let myReadStream = fs.createReadStream(__dirname + '../pages/home_page.html', 'utf-8');
+  let myReadStream = fs.createReadStream(__dirname + '/../pages/home.html', 'utf-8');
   myReadStream.pipe(res);
 });
 
